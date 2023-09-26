b9ab9d0d4b05d118c21ad9841567ce2f82e6603f
Dave Cadelina <dave.cadelina@reckon.com>
2023-09-27 09:10:04 +1000

    added fontawesome

--- App.tsx
+++ App.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, StyleSheet, TouchableOpacity, View} from 'react-native';
 import Title from './components/Title/Title';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
 
 const App = (): JSX.Element => {
   return (
     <SafeAreaView>
-      <Title title="Let's Explore" />
+      <View style={style.container}>
+        <Title title="Let's Explore" />
+        <TouchableOpacity style={style.iconContainer}>
+          <FontAwesomeIcon icon={faEnvelope} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
+
+const style = StyleSheet.create({
+  container: {
+    marginLeft: 27,
+    marginRight: 17,
+    marginTop: 30,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  iconContainer: {
+    padding: 10,
+  },
+});
 
 export default App;
