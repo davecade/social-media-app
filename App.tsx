bed522a9e129010d55587c5cf186f9d1c7c752f7
Dave Cadelina <dave.cadelina@reckon.com>
2023-09-28 11:53:07 +1000

    added message icon

--- App.tsx
+++ App.tsx
@@ -1,34 +1,24 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, TouchableOpacity, View} from 'react-native';
+import {SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
 import Title from './components/Title/Title';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
+import globalStyles from './assets/styles/globalStyles';
 
 const App = (): JSX.Element => {
   return (
     <SafeAreaView>
-      <View style={style.container}>
+      <View style={globalStyles.container}>
         <Title title="Let's Explore" />
-        <TouchableOpacity style={style.iconContainer}>
-          <FontAwesomeIcon icon={faEnvelope} />
+        <TouchableOpacity style={globalStyles.messageIcon}>
+          <FontAwesomeIcon icon={faEnvelope} size={20} color={'#898DAE'} />
+          <View style={globalStyles.messageNumberContainer}>
+            <Text style={globalStyles.messageNumber}>2</Text>
+          </View>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 };
-
-const style = StyleSheet.create({
-  container: {
-    marginLeft: 27,
-    marginRight: 17,
-    marginTop: 30,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  iconContainer: {
-    padding: 10,
-  },
-});
 
 export default App;
---
+++ assets/styles/globalStyles.ts
@@ -0,0 +1,37 @@
+import {StyleSheet} from 'react-native';
+import {getFontFamily} from '../fonts/helper';
+
+const globalStyles = StyleSheet.create({
+  container: {
+    marginLeft: 27,
+    marginRight: 17,
+    marginTop: 30,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  messageIcon: {
+    padding: 10,
+    backgroundColor: '#F9FAFB',
+    borderRadius: 50,
+  },
+  messageNumberContainer: {
+    backgroundColor: '#F35BAC',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    width: 10,
+    height: 10,
+    borderRadius: 50,
+    position: 'absolute',
+    right: 7,
+    top: 9,
+  },
+  messageNumber: {
+    color: '#FFF',
+    fontSize: 6,
+    fontFamily: getFontFamily('Inter', '600'),
+  },
+});
+
+export default globalStyles;
